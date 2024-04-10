f9395d966efdb64dbd29f7b5146e0b6c74768d80
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-04-10 15:44:56 -0400

    Move to my own Repository
    Move to my own repository of Window programming Assingment5

---
+++ WP 05 - SERVER/WP_A05_ServerApp/Program.cs
@@ -0,0 +1,34 @@
+﻿/**
+* FILE				: Program.cs
+* PROJECT			: PROG 2121 - Windows Programming Assignment 05
+* PROGRAMMERS		:
+*   Minchul Hwang  ID: 8818858
+* FIRST VERSION		: Nov. 19, 2023
+* DESCRIPTION		: This program is a server program that is linked to the WP_A05_ServerApp program.
+*                     The server connected to the client is a single server and uses multithreading.
+*                     
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WP_A05_ServerApp
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Server server = new Server();
+            await server.StartListenerAsync();
+        }
+    }
+
+    
+}
